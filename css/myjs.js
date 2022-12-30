cf045c3619e1e5bd1344a7e96af38ae51365f016
mdiliyas77 <mdiliyas.mugli@yahoo.com>
2022-12-31 04:11:42 +0530

    Self

--- css/myjs.js
+++ css/myjs.js
@@ -1,15 +1,33 @@
+
 function c() {
-    var c = document.getElementById("c");
-    var cpp = document.getElementById("cpp");
-    var java = document.getElementById("java");
-    var csharp = document.getElementById("csharp");
-    var python = document.getElementById("python");
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
     if (c.style.display === "none") {
         c.style.display = "block";
         cpp.style.display = "none";
         java.style.display = "none";
         csharp.style.display = "none";
         python.style.display = "none";
+        css.style.display = "none";
+        html.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+
 
 
     }
@@ -19,17 +37,33 @@ function c() {
 }
 
 function cpp() {
-    var cpp = document.getElementById("cpp");
-    var c = document.getElementById("c");
-    var java = document.getElementById("java");
-    var csharp = document.getElementById("csharp");
-    var python = document.getElementById("python");
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
     if (cpp.style.display === "none") {
         cpp.style.display = "block";
         c.style.display = "none";
         java.style.display = "none";
         csharp.style.display = "none";
         python.style.display = "none";
+        css.style.display = "none";
+        html.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
     }
     else {
         cpp.style.display = "none";
@@ -37,17 +71,33 @@ function cpp() {
 }
 
 function java() {
-    var cpp = document.getElementById("cpp");
-    var c = document.getElementById("c");
-    var java = document.getElementById("java");
-    var csharp = document.getElementById("csharp");
-    var python = document.getElementById("python");
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
     if (java.style.display === "none") {
         java.style.display = "block";
         cpp.style.display = "none";
         c.style.display = "none";
         csharp.style.display = "none";
         python.style.display = "none";
+        css.style.display = "none";
+        html.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
     }
     else {
         java.style.display = "none";
@@ -55,17 +105,33 @@ function java() {
 }
 
 function csharp() {
-    var cpp = document.getElementById("cpp");
-    var c = document.getElementById("c");
-    var java = document.getElementById("java");
-    var csharp = document.getElementById("csharp");
-    var python = document.getElementById("python");
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
     if (csharp.style.display === "none") {
         csharp.style.display = "block";
         cpp.style.display = "none";
         c.style.display = "none";
         java.style.display = "none";
         python.style.display = "none";
+        css.style.display = "none";
+        html.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
     }
     else {
         csharp.style.display = "none";
@@ -73,19 +139,310 @@ function csharp() {
 }
 
 function python() {
-    var python = document.getElementById("python");
-    var cpp = document.getElementById("cpp");
-    var c = document.getElementById("c");
-    var java = document.getElementById("java");
-    var csharp = document.getElementById("csharp");
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
     if (python.style.display === "none") {
         python.style.display = "block";
         csharp.style.display = "none";
         cpp.style.display = "none";
         c.style.display = "none";
         java.style.display = "none";
+        css.style.display = "none";
+        html.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
     }
     else {
         python.style.display = "none";
     }
 }
+
+function html() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (html.style.display === "none") {
+        html.style.display = "block";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        html.style.display = "none";
+    }
+}
+
+
+function css() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (css.style.display === "none") {
+        css.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        css.style.display = "none";
+    }
+}
+
+function js() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (js.style.display === "none") {
+        js.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        bootstrap.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        js.style.display = "none";
+    }
+}
+
+function jquery() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (jquery.style.display === "none") {
+        jquery.style.display = "block";
+        vue.style.display = "none";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        bootstrap.style.display = "none";
+    }
+    else {
+        jquery.style.display = "none";
+    }
+}
+
+
+function bootstrap() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (bootstrap.style.display === "none") {
+        bootstrap.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        bootstrap.style.display = "none";
+    }
+}
+
+function angular() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (angular.style.display === "none") {
+        angular.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        bootstrap.style.display = "none";
+        react.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        angular.style.display = "none";
+    }
+}
+
+function react() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (react.style.display === "none") {
+        react.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        angular.style.display = "none";
+        bootstrap.style.display = "none";
+        vue.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        react.style.display = "none";
+    }
+}
+
+function vue() {
+    var c = document.getElementById("ccontent");
+    var cpp = document.getElementById("cppcontent");
+    var java = document.getElementById("javacontent");
+    var csharp = document.getElementById("csharpcontent");
+    var python = document.getElementById("pythoncontent");
+    var html = document.getElementById("htmlcontent");
+    var css = document.getElementById("csscontent");
+    var js = document.getElementById("jscontent");
+    var jquery = document.getElementById("jquerycontent");
+    var bootstrap = document.getElementById("bootstrapcontent");
+    var angular = document.getElementById("angularcontent");
+    var react = document.getElementById("reactcontent");
+    var vue = document.getElementById("vuecontent");
+    if (vue.style.display === "none") {
+        vue.style.display = "block";
+        html.style.display = "none";
+        python.style.display = "none";
+        csharp.style.display = "none";
+        cpp.style.display = "none";
+        c.style.display = "none";
+        java.style.display = "none";
+        css.style.display = "none";
+        js.style.display = "none";
+        angular.style.display = "none";
+        react.style.display = "none";
+        bootstrap.style.display = "none";
+        jquery.style.display = "none";
+    }
+    else {
+        vue.style.display = "none";
+    }
+}
+
